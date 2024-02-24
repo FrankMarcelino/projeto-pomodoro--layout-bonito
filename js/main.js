1bdfeb95d729a1d4004e82edf7f8768f00d1b239
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-24 11:33:15 -0300

    já mexi muito no codigo

--- js/main.js
+++ js/main.js
@@ -1,79 +1,115 @@
-const circularProgressBar = document.querySelector("#circularProgressBar");
-const circularProgressBarNumber = document.querySelector("#circularProgressBar .progress-value");
-const buttonTypePomodoro = document.querySelector("#buttonTypePomodoro");
-const buttonTypeShortBreak = document.querySelector("#buttonTypeShortBreak");
+document.addEventListener('DOMContentLoaded', () => {
+    const circularProgressBar = document.querySelector("#circularProgressBar");
+    const circularProgressBarNumber = document.querySelector("#circularProgressBar .progress-value");
+    const buttonTypePomodoro = document.querySelector("#buttonTypePomodoro");
+    const buttonTypeShortBreak = document.querySelector("#buttonTypeShortBreak");
 
-const audio = new Audio('alarm.mp3');
-
-const pomodoroTimerInSeconds = 1500; 
-const shortBreakTimerInSeconds = 300;
-const TIMER_TYPE_POMODORO = 'POMODORO';
-const TIMER_TYPE_SHORT_BREAK = 'SHORTBREAK';
+    const audio = new Audio('alarm.mp3');
 
 
-let progressInterval;
-let pomodoroType = TIMER_TYPE_POMODORO;
-let timerValue = pomodoroTimerInSeconds;
-let multiplierFactor = 360 / timerValue;
+    const formInputs = document.querySelector("#form-inputs");
+    const inputPomodoro = parseFloat(document.getElementById("input-pomodoro").value);
+    const inputShortBreak = document.querySelector("#input-short-break");
+
+    formInputs.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const pomodoro = parseFloat(document.getElementById("input-pomodoro").value);
+        console.log(pomodoro)
+    })
 
 
-function formatNumberInStringMinute(number){
+
+
+
+
+
+
+
+
+    const pomodoroTimerInSeconds = 1500;
+    const shortBreakTimerInSeconds = 300;
+
+
+
+    const TIMER_TYPE_POMODORO = 'POMODORO';
+    const TIMER_TYPE_SHORT_BREAK = 'SHORTBREAK';
+
+
+
+
+
+    let progressInterval;
+    let pomodoroType = TIMER_TYPE_POMODORO;
+    let timerValue = pomodoroTimerInSeconds;
+    let multiplierFactor = 360 / timerValue;
+
+
+    function formatNumberInStringMinute(number) {
+
+        const minutes = Math.trunc(number / 60)
+            .toString()
+            .padStart(2, '0');
+        const seconds = Math.trunc(number % 60)
+            .toString()
+            .padStart(2, '0');
+        return `${minutes}:${seconds}`;
+    }
+
+    const startTimer = () => {
+        progressInterval = setInterval(() => {
+            timerValue--;
+            setInfoCircularProgressBar();
+        }, 1000);
+    }
+
+    const stopTimer = () =>  clearInterval(progressInterval);
     
-    const minutes = Math.trunc(number / 60)
-                        .toString()
-                            .padStart(2, '0');
-    const seconds = Math.trunc(number % 60)
-                            .toString()
-                                .padStart(2, '0');
-    return `${minutes}:${seconds}`;
-}
+    const resetTimer = () => {
+        clearInterval(progressInterval);
 
-const startTimer = () => {
-    progressInterval = setInterval(() => {
-        timerValue--;
+        timerValue = (pomodoroType === TIMER_TYPE_POMODORO)
+            ? pomodoroTimerInSeconds
+            : shortBreakTimerInSeconds;
+
+        multiplierFactor = 360 / timerValue;
+
         setInfoCircularProgressBar();
-    }, 1000);
-}
-
-const stopTimer = () => clearInterval(progressInterval);
-
-const resetTimer = () =>{
-    clearInterval(progressInterval);
-
-    timerValue = (pomodoroType === TIMER_TYPE_POMODORO) 
-                        ? pomodoroTimerInSeconds 
-                            : shortBreakTimerInSeconds;
-
-    multiplierFactor = 360 / timerValue;
-
-    setInfoCircularProgressBar();    
-    // audio.stop();
-}
-  
-function setInfoCircularProgressBar(){
-
-    if(timerValue === 0){
-        stopTimer();
-        audio.play();        
+        // audio.stop();
     }
 
-    circularProgressBarNumber.textContent = `${formatNumberInStringMinute(timerValue) }`;
-    circularProgressBar.style.background = `conic-gradient(var(--blue) ${timerValue * multiplierFactor}deg, var(--purple) 0deg)`;
+    const btnStartTimer = document.querySelector("#buttonStart");
+    const btnStopTimer = document.querySelector("#buttonStop");
+    const btnResetTimer = document.querySelector("#buttonReset");
 
-}
+    btnStartTimer.addEventListener("click", startTimer);  
+    btnStopTimer.addEventListener("click", stopTimer);
+    btnResetTimer.addEventListener("click", resetTimer);
 
-const setPomodoroType = (type) =>{    
-    pomodoroType = type; 
+    function setInfoCircularProgressBar() {
 
-    if(type === TIMER_TYPE_POMODORO){
-        buttonTypeShortBreak.classList.remove("active");
-        buttonTypePomodoro.classList.add("active");                  
-    }else{
-        buttonTypePomodoro.classList.remove("active");
-        buttonTypeShortBreak.classList.add("active"); 
+        if (timerValue === 0) {
+            stopTimer();
+            audio.play();
+        }
+
+        circularProgressBarNumber.textContent = `${formatNumberInStringMinute(timerValue)}`;
+        circularProgressBar.style.background = `conic-gradient(var(--blue) ${timerValue * multiplierFactor}deg, var(--purple) 0deg)`;
+
     }
 
-    resetTimer();
-}
+    const setPomodoroType = (type) => {
+        pomodoroType = type;
 
+        if (type === TIMER_TYPE_POMODORO) {
+            buttonTypeShortBreak.classList.remove("active");
+            buttonTypePomodoro.classList.add("active");
+        } else {
+            buttonTypePomodoro.classList.remove("active");
+            buttonTypeShortBreak.classList.add("active");
+        }
 
+        resetTimer();
+    }
+
+})
